fb694e0f9e28c74bfd6f3fc1ab8de98153e59c55
Noah Stuertz <noah@coru.sh>
2024-06-24 14:21:13 +0200

    removed unnecessary code and added some documentation

--- src/nws.cc
+++ src/nws.cc
@@ -14,8 +14,19 @@ const int MATCH_SCORE = 1;
 const int MISMATCH_SCORE = -1;
 const int GAP_PENALTY = -2;
 
+/**
+ * @brief helper function to sort the tiled matrix by its position
+ */
 bool compare(tile a, tile b) { return (a.i + a.j) > (b.i + b.j); }
 
+/**
+ * @brief Tile the n x m matrix into tile_size large parts
+ * 
+ * @param n the length of sequence 1
+ * @param m the length of sequence 2
+ * @param tile_size how large is each side of the tile supposed to be
+ * @return std::vector<tile>  the tiled matrix
+ */
 std::vector<tile> get_tiled_matrix(int n, int m, int tile_size) {
   std::vector<tile> matrix;
   for (int i = 1; i <= n; i += tile_size) {
@@ -44,6 +55,8 @@ std::vector<tile> get_tiled_matrix(int n, int m, int tile_size) {
 }
 
 int main(int argc, char *argv[]) {
+  // Select the device sycl is supposed to use
+  // try gpu first and cpu if not applicable
   sycl::device d;
   try {
     d = sycl::device(sycl::gpu_selector_v);
@@ -54,9 +67,9 @@ int main(int argc, char *argv[]) {
   }
 
   std::cout << "Using " << d.get_info<sycl::info::device::name>();
-  std::cout << std::endl;
+  std::cout << std::endl << std::endl;
 
-  // These can be turned into arguments
+  // first two arguments are read as sequences
   std::string seq1 = argv[1];
   std::string seq2 = argv[2];
   int tile_size = 3;
@@ -71,6 +84,8 @@ int main(int argc, char *argv[]) {
   std::vector<int> result(n * m);
   sycl::buffer result_buffer(result.data(), sycl::range<2>{n, m});
 
+
+  // Initialize the first row and column of the matrix statically
   for (int i = 0; i < n; i++) {
     result[i * m] = i * GAP_PENALTY;
   }
@@ -84,7 +99,7 @@ int main(int argc, char *argv[]) {
     std::vector<tile> subset;
     int counter = (M.back().i + M.back().j);
 
-    // Split the matrix into lines
+    // Split the matrix into lines, so called subsets
     do {
       if (M.empty()) {
         break;
@@ -96,11 +111,13 @@ int main(int argc, char *argv[]) {
     if (!subset.empty()) {
       sycl::buffer<tile, 1> buffer(subset.data(),
                                    sycl::range<1>{subset.size()});
+      // This is the part that actually does the calculations
       q.submit([&](sycl::handler &cgh) {
-        // auto out = sycl::stream(1024, 768, cgh);
+        // create accessors for the buffers we defined earlier
         auto aSubset = buffer.get_access<sycl::access::mode::read>(cgh);
         auto aResult = result_buffer.get_access<sycl::access::mode::write>(cgh);
 
+        // parallel version of the for loop in sycl
         cgh.parallel_for(sycl::range<1>(aSubset.size()), [=](sycl::id<1> idx) {
           for (int x = 0; x < aSubset[idx].xsize; x++) {
             for (int y = 0; y < aSubset[idx].ysize; y++) {
@@ -120,7 +137,7 @@ int main(int argc, char *argv[]) {
       q.wait();
     }
   }
-  result_buffer.set_write_back();
+
   std::cout << "Seq1: " + seq1 << std::endl << "Seq2: " + seq2 << std::endl;
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
