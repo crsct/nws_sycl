320265dfddb45edb4793dbfb67badf18bf317d29
Noah Stuertz <noah@coru.sh>
2024-06-24 12:05:12 +0200

    nixified opensycl

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Bellman's GAP compiler";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    systems.url = "github:nix-systems/default";
+    flake-utils.url = "github:numtide/flake-utils";
+    sycl.url = "github:r-ryantm/nixpkgs/auto-update/opensycl";
+  };
+
+  outputs =
+    { self
+    , systems
+    , nixpkgs
+    , flake-utils
+    , sycl
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+      let
+        overlay = final: prev: {
+          inherit (sycl.legacyPackages.${prev.system})
+            opensycl;
+        };
+        pkgs = (nixpkgs.legacyPackages.${system}.extend overlay);
+
+      in
+      {
+        # overlay = overlay;
+        devShells.default = import ./shell.nix { inherit pkgs; };
+      });
+}
---
+++ shell.nix
@@ -0,0 +1,27 @@
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+mkShell {
+  buildInputs = [
+    nixpkgs-fmt
+    opensycl
+    gnumake
+    python3
+    clang-tools
+    clang
+    gdb
+
+    llvmPackages_latest.llvm
+    llvmPackages_latest.libcxxClang
+    llvmPackages_latest.libcxxStdenv
+    llvmPackages_latest.bintools
+    llvmPackages_latest.openmp
+
+    lld
+    pkg-config
+  ];
+
+  ACPP_TARGETS = "omp.library-only";
+  LD_LIBRARY_PATH = "";
+  CC = "clang";
+  CXX = "clang++";
+}
