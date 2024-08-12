28990bd3122560aabb42af9e98fe7e3fecb6663a
Ubuntu <ubuntu@noblechickadee-e73d2.denbi.local>
2024-08-12 11:47:01 +0000

    Functioning sycl code (oneapi)

--- src/nws.cc
+++ src/nws.cc
@@ -1,10 +1,16 @@
 #include "nws.hh"
 
+#include <fstream> 
+#include <iostream> 
+#include <ostream>
+#include <string> 
+
 using namespace sycl;
 
 struct tile {
   int i;
   int j;
+
   int xsize;
   int ysize;
 };
@@ -27,29 +33,26 @@ bool compare(tile a, tile b) { return (a.i + a.j) > (b.i + b.j); }
  * @param tile_size how large is each side of the tile supposed to be
  * @return std::vector<tile>  the tiled matrix
  */
-std::vector<tile> get_tiled_matrix(int n, int m, int tile_size) {
+std::vector<tile> get_tiled_matrix(std::string seq1, std::string seq2, int tile_size) {
   std::vector<tile> matrix;
-  for (int i = 1; i <= n; i += tile_size) {
-    for (int j = 1; j <= m; j += tile_size) {
+  int n = seq1.size();
+  int m = seq2.size();
+
+  for (int i = 0; i <= n; i += tile_size) {
+    for (int j = 0; j <= m; j += tile_size) {
+
       tile t;
       t.i = i;
       t.j = j;
 
-      if (i <= n - tile_size) {
-        t.xsize = tile_size;
-      } else {
-        t.xsize = (n - i) % tile_size;
-      }
 
-      if (j <= m - tile_size) {
-        t.ysize = tile_size;
-      } else {
-        t.ysize = (m - j) % tile_size;
-      }
+      t.xsize = std::min(tile_size, (int(n) + 1) - i);
+      t.ysize = std::min(tile_size, (int(m) + 1) - j);
 
       matrix.push_back(t);
     }
   }
+  
   sort(matrix.begin(), matrix.end(), compare);
   return matrix;
 }
@@ -66,40 +69,54 @@ int main(int argc, char *argv[]) {
     d = sycl::device(sycl::cpu_selector_v);
   }
 
-  std::cout << "Using " << d.get_info<sycl::info::device::name>();
-  std::cout << std::endl << std::endl;
 
-  // first two arguments are read as sequences
-  std::string seq1 = argv[1];
-  std::string seq2 = argv[2];
+  std::string seq1, seq2, line;
 
-  std::vector<char> charSeq1(seq1.begin(), seq1.end());
-  std::vector<char> charSeq2(seq2.begin(), seq2.end());
+  int tile_size = atoi(argv[1]);
+  int offset = 0;
+  if (tile_size == 0) {
+    std::cout << "Assuming a tile size of 64x64" << std::endl;
+    tile_size = 64;
+    offset = 1;
+  }
 
-  int tile_size = 3;
+  std::ifstream inputFile(argv[2 - offset]);
+  if (!inputFile.is_open()) {
+    std::cerr << "Error reading file " << argv[2 - offset] << std::endl;
+    return 1;
+  }
+
+  if (argc >= 4 - offset) {
+    std::ifstream inputFile2(argv[3 - offset]);
+    if (!inputFile2.is_open()) {
+      std::cerr << "Error reading file " << argv[3 - offset] << std::endl;
+      return 1;
+    }
+    while (getline(inputFile, line)) {
+      seq1 += line;
+    }
+        while (getline(inputFile2, line)) {
+      seq2 += line;
+    }
+  } else {
+    while (getline(inputFile, line)) {
+      seq1 += line;
+      seq2 += line;
+    }
+  }
 
   size_t n = seq1.size() + 1;
   size_t m = seq2.size() + 1;
 
-  std::vector<tile> M = get_tiled_matrix(n, m, tile_size);
+
+  std::vector<tile> M = get_tiled_matrix(seq1, seq2, tile_size);
 
   sycl::queue q;
 
   sycl::buffer<int, 1> results(sycl::range<1>(n*m));
 
-  // Initialize the first row and column of the matrix statically
-  {
-    sycl::host_accessor result{results};
-    for (int i = 0; i < n; i++) {
-      result[i * m] = i * GAP_PENALTY;
-    }
-    for (int j = 0; j < m; j++) {
-      result[j] = j * GAP_PENALTY;
-    }
-  }
-
   // Setup subsets (rows in rotated matrix)
-  for (int i = 0; i < std::floor((n + m) / 3); i++) {
+  for (int i = 0; i < std::ceil((n + m) / tile_size) + 1; i++) {
     std::vector<tile> subset;
     int counter = (M.back().i + M.back().j);
 
@@ -113,11 +130,11 @@ int main(int argc, char *argv[]) {
     } while ((M.back().i + M.back().j) == counter);
 
     if (!subset.empty()) {
+      sycl::buffer<char, 1> bSeq1(seq1.data(), sycl::range<1>{seq1.size()});
+      sycl::buffer<char, 1> bSeq2(seq2.data(), sycl::range<1>{seq2.size()});
       sycl::buffer<tile, 1> bSubset(subset.data(),
                                    sycl::range<1>{subset.size()});
 
-      sycl::buffer<char, 1> bSeq1(charSeq1.data(), charSeq1.size());
-      sycl::buffer<char, 1> bSeq2(charSeq2.data(), charSeq2.size());
 
       // This is the part that actually does the calculations
       q.submit([&](sycl::handler &cgh) {
@@ -127,6 +144,7 @@ int main(int argc, char *argv[]) {
         auto aSeq1 = sycl::accessor{bSeq1, cgh, sycl::read_only};
         auto aSeq2 = sycl::accessor{bSeq2, cgh, sycl::read_only};
 
+        sycl::stream os(1024, 128, cgh);
         // parallel version of the for loop in sycl
         cgh.parallel_for(sycl::range<1>(aSubset.size()), [=](sycl::id<1> idx) {
           for (int x = 0; x < aSubset[idx].xsize; x++) {
@@ -134,12 +152,23 @@ int main(int argc, char *argv[]) {
               int posx = aSubset[idx].i + x;
               int posy = aSubset[idx].j + y;
 
-              int sub = aResult[(posx - 1) * m + posy - 1] +
-                        (aSeq1[posx - 1] == aSeq2[posy - 1] ? MATCH_SCORE
+              if (posx > 0 && posy > 0) {
+                char char1 = aSeq1[posx-1];
+                char char2 = aSeq2[posy-1];
+                int sub = aResult[(posx - 1) * m + posy - 1] +
+                        (char1 == char2 ? MATCH_SCORE
                                                           : MISMATCH_SCORE);
-              int del = aResult[(posx - 1) * m + posy] + GAP_PENALTY;
-              int ins = aResult[posx * m + posy - 1] + GAP_PENALTY;
-              aResult[posx * m + posy] = std::max({sub, del, ins});
+                int del = aResult[(posx - 1) * m + posy] + GAP_PENALTY;
+                int ins = aResult[posx * m + posy - 1] + GAP_PENALTY;
+                int result = std::max({sub, del, ins});
+                aResult[posx * m + posy] = result;
+              } else if (posx > 0) {
+                aResult[posx * m + posy] = aResult[(posx - 1) * m + posy] + GAP_PENALTY;
+              } else if (posy > 0) {
+                aResult[posx * m + posy] = aResult[posx * m + posy - 1] + GAP_PENALTY;
+              } else {
+                aResult[posx * m + posy] = 0;
+              }
             }
           }
         });
@@ -148,12 +177,15 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  // std::cout << std::endl;
+  // std::cout << std::endl;
   sycl::host_accessor result{results};
-  std::cout << "Seq1: " + seq1 << std::endl << "Seq2: " + seq2 << std::endl;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      std::cout << result[i * m + j] << " ";
-    }
-    std::cout << std::endl;
-  }
+  // for (int i = 0; i < n; i++) {
+  //   for (int j = 0; j < m; j++) {
+  //     std::cout << result[i * m + j] << " ";
+  //   }
+  //   std::cout << std::endl;
+  // }
+  std::cout << result[(n-1) * m + (m-1)];
+
 }
